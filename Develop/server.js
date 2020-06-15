d54f8382edf52ce798a7d826ef4495be190a435b
Aaron <aaronkh19@gmail.com>
2020-06-14 21:45:38 -0600

    finished setting up routes

--- Develop/server.js
+++ Develop/server.js
@@ -11,9 +11,34 @@ const fs = require("fs");
 const app = express();
 const port = 8080;
 
+//const for path to the main directory to make path shorter and quicker
+const mainDir = path.join(__dirname, "/public");
+
 //Empty array for notes
 let notes = [];
 
 //Using the express app middleware to parse the data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+//setting up routes
+//route to notes page
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  let savedNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(savedNote[Number(req.params.id)]);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(mainDir, "index.html"));
+});
+
+//functions for writing the note to the json file
